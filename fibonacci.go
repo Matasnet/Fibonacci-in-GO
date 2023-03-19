77b9738af3231a4a4374d9738c28a899afe24a00
Mateusz Kozera <Matas.wp@gmail.com>
2023-03-19 11:30:17 +0100

    Create fibonacci.go

---
+++ fibonacci.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+
+func Fibonacci(n int) []int {
+	if n < 2 {
+		return make([]int, 0)
+	}
+	nums := make([]int, n)
+	nums[0], nums[1] = 1, 1
+
+	for i := 2; i < n; i++ {
+		nums[i] = nums[i-1] + nums[i-2]
+	}
+	
+	
+	return nums
+}
+
+
+func main() {
+	var num int
+	fmt.Print("What's the Fibonacci sequence you want? >>> ")
+	fmt.Scanln(&num)
+	fmt.Println("The Fibonacci sequence is: ", Fibonacci(num))
+}
